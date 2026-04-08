05cca3639a62af29606330eba0e02d80fa30f3ae
Aaron Siu <aarons3209@gmail.com>
2026-04-08 17:31:39 -0400

    Read me completion

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,15 @@
 #Fast API 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from app.api import videos
+from app.api import videos, clips, stats
 
 #Create API Server
 app = FastAPI(title="Volley Pro API")
 
-#Register Routes
+#Register Routes to be called from frontend
 app.include_router(videos.router, prefix="/videos")
-#app.include_router(clips.router, prefix="/clips")
-#app.include_router(stats.router, prefix="/stats")
+app.include_router(clips.router, prefix="/clips")
+app.include_router(stats.router, prefix="/stats")
 
 #Serve frames as static images/files
 app.mount("/frames", StaticFiles(directory="frames"), name="frames")
